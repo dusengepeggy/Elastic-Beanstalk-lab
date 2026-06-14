6ac55e32a0727d5ed3b1be0fea1e0f01685739d2
dusengepeggy <dickinepeggy100@gmail.com>
2026-06-14 08:42:02 +0200

    project setup

---
+++ src/main/java/com/fooddelivery/src/controllers/OrderController.java
@@ -0,0 +1,12 @@
+package com.fooddelivery.src.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Peggy's delivery app";
+    }
+}
